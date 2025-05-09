d8638cb3fcd0dde8060fb2ca55aefb8eff845f38
entr0py42 <mehmet-tr@outlook.com.tr>
2025-05-09 15:36:10 +0300

    partial internationalisation fix

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -18,7 +18,8 @@ export default async function LocaleLayout({
   params
 } : {
   children: React.ReactNode;
-  params: Promise<{ locale : string }>;
+  params: { locale: string }
+
 }) {
   const {locale} = await params
 
